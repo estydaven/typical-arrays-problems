f40c05d13d652fd0f2cb501c9a8483923909e9ec
estydaven <esetnod1111@gmail.com>
2021-03-03 18:00:17 +0300

    fix: fix typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 
 exports.min = function min (array) {
-  if (array.length === 0 || !array){
+  if (!array || array.length === 0){
     return 0;
   }
   return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  if (array.length === 0 || !array){
+  if (!array || array.length === 0){
     return 0;
   }
   return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  if (array.length === 0 || !array){
+  if (!array || array.length === 0){
     return 0;
   }
   return (array.reduce((a, b) => (a + b)) / array.length);
