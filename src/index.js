170ffd7dd5b397b8409e5189b5d0684787c99cb6
estydaven <esetnod1111@gmail.com>
2021-03-04 17:16:46 +0300

    refactor: change typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -3,19 +3,37 @@ exports.min = function min (array) {
   if (!array || array.length === 0){
     return 0;
   }
-  return Math.min(...array);
+  let arrMin = array.reduce((acc, item) => {
+    if (item < acc){
+      acc = item;
+    }
+    return acc;
+  });
+  return arrMin;
 }
 
 exports.max = function max (array) {
   if (!array || array.length === 0){
     return 0;
   }
-  return Math.max(...array);
+  let arrMax = array.reduce((acc, item) => {
+    if (item > acc){
+      acc = item;
+    }
+    return acc;
+  });
+  return arrMax;
 }
 
 exports.avg = function avg (array) {
   if (!array || array.length === 0){
     return 0;
   }
-  return (array.reduce((a, b) => (a + b)) / array.length);
+  
+  let arrAvg = array.reduce((acc, item) => {
+    acc += item;
+    return acc;
+  });
+  let numAvg = arrAvg / array.length;
+  return numAvg;
 }
